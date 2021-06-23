7517ef20437eeb1d93348e3764da38fd6ae72280
Badr <70712849+bkhaie@users.noreply.github.com>
2021-06-24 00:15:28 +0200

    tictak

--- JS/expando.js
+++ JS/expando.js
@@ -196,6 +196,15 @@ function openCalculator() {
   }
 }
 
+function openTictak() {
+  var x = document.getElementById("toggle2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
 /*---- Toggle */
 
 
