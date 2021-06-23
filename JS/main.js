59dcdc5c470e2ff561007bc9e7fde0ad6a781b30
Badr <70712849+bkhaie@users.noreply.github.com>
2021-06-24 00:49:42 +0200

    Updated

--- JS/main.js
+++ JS/main.js
@@ -45,12 +45,4 @@
 
 
 /*Read more button*/
-function myWarn() {
-	var txt;
-	if (confirm("Wilt u echt naar een ander pagina?")) document.location = "Projecten/Bloemenwinkel/Sprint1.html"; {
-	}
-  }
-
-
-
 
