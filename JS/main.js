93f8c0e366280f2f383a29bebec6c09f1f661aff
Badr <70712849+bkhaie@users.noreply.github.com>
2021-06-24 16:11:00 +0200

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -62,16 +62,14 @@ const startNewGame = () => {
 cells.forEach(cell => cell.addEventListener("click", markCell))
 document.getElementById("Newgame").addEventListener("click", startNewGame)
 
-/*Readmore button*/
 
-/*warn button*/
 
 function bloemen() {
-	if (confirm("Wilt u echt naar een ander pagina?")) document.location = "Projecten/Bloemenwinkel/Sprint1.html"; 
-	}
+if (confirm("Wilt u echt naar een ander pagina?")) document.location = "Projecten/Bloemenwinkel/Sprint1.html"; 
+}
 
-	function Rekenmachine() {
-	if (confirm("Wilt u echt naar een ander pagina?")) document.location = "Projecten/Rekenmachine/Calculator Project/Calculator v3/MijnErvaringen.html"; 
+function Rekenmachine() {
+if (confirm("Wilt u echt naar een ander pagina?")) document.location = "Projecten/Rekenmachine/Calculator Project/Calculator v3/MijnErvaringen.html"; 
 	  }
 	
 
