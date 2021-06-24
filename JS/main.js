7ca05fe33b4c907e4c36f97f145e0723d07b0c30
Badr <70712849+bkhaie@users.noreply.github.com>
2021-06-24 13:07:57 +0200

    updated

--- JS/main.js
+++ JS/main.js
@@ -44,10 +44,14 @@
 
 
 
-/*Read more button*/
+/*warn button*/
 
-function myWarn() {
-	var txt;
-	if (confirm("Wilt u echt naar een ander pagina?")) document.location = "Projecten/Bloemenwinkel/Sprint1.html"; {
+function bloemen() {
+	if (confirm("Wilt u echt naar een ander pagina?")) document.location = "Projecten/Bloemenwinkel/Sprint1.html"; 
 	}
-  }
+
+	function Rekenmachine() {
+	if (confirm("Wilt u echt naar een ander pagina?")) document.location = "Projecten/Rekenmachine/Calculator Project/Calculator v3/MijnErvaringen.html"; 
+	  }
+	
+
