154383cd42a28b2e079014f7f6862c2ad14f118b
Badr <70712849+bkhaie@users.noreply.github.com>
2021-06-24 21:38:32 +0200

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,32 +1,39 @@
 function bloemen() {
-	var txt;
+	const txt;
 	if (confirm("Wilt u echt naar een ander pagina?")) document.location = "Projecten/Bloemenwinkel/Sprint1.html"; {
 	}
   }
 
 function Rekenmachine() {
-var txt;
-if (confirm("Wilt u echt naar een ander pagina?")) document.location = "Projecten/Rekenmachine/Calculator Project/Calculator v3/MijnErvaringen.html"; {
+	const txt;
+	if (confirm("Wilt u echt naar een ander pagina?")) document.location = "Projecten/Rekenmachine/Calculator Project/Calculator v3/MijnErvaringen.html"; {
 }}
 
 
 function boterkaas() {
-	var txt;
+	const txt;
 	if (confirm("Wilt u echt naar een ander pagina?")) document.location = "Projecten/BKE/index.html"; {
 }}
 
 
-function kaartspel() {
-	var txt;
-	if (confirm("Weet u echt het spel downloaden?")) document.location = "Projecten/Hoger-Lager-main/Hoger-Lager-main.7z"; 
+
+ function dicespel(){
+  const password = prompt('Voer het wachtwoord in:');
+  if(password.toLowerCase() == "khaie"){
+    window.open("Projecten/Dicegame/dicegame.7z")    
+  }else{
+    alert("Foute wachtwoord!! Probeer opnieuw");
+  }
 }
 
-
-function dicespel() {
-	var txt;
-	if (confirm("Weet u echt het spel downloaden?")) document.location = "Projecten/Dicegame/dicegame.7z"; 
-}
-
+function kaartspel(){
+	const password = prompt('Voer het wachtwoord in:');
+	if(password.toLowerCase() == "khaie"){
+	  window.open("Projecten/Hoger-Lager-main/Hoger-Lager-main.7z")    
+	}else{
+	  alert("Foute wachtwoord!! Probeer opnieuw");
+	}
+  }
 
 
 
