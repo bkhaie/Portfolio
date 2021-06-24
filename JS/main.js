3efd2e438c6451e95874e4e3f771f3e29d7eea4c
Badr <70712849+bkhaie@users.noreply.github.com>
2021-06-24 16:49:53 +0200

    updated

--- JS/main.js
+++ JS/main.js
@@ -8,3 +8,9 @@ function Rekenmachine() {
 var txt;
 if (confirm("Wilt u echt naar een ander pagina?")) document.location = "Projecten/Rekenmachine/Calculator Project/Calculator v3/MijnErvaringen.html"; {
 }}
+
+
+function bko() {
+	var txt;
+	if (confirm("Wilt u echt naar een ander pagina?")) document.location = "Projecten/bko/index.html"; {
+}}
