f8b0205c9abfb2d99427349c3186ba31a72bbc7f
Badr <70712849+bkhaie@users.noreply.github.com>
2021-10-24 23:38:28 +0200

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -23,7 +23,7 @@ function pizzames() {
 function healthone(){
 	const password = prompt('Voer het wachtwoord in:');
 	if(password.toLowerCase() == "khaie"){
-		document.location = "Projecten/healthone/Templates/home.php";
+		document.location = "Projecten/healthone/Templates/index.html";
 	}else{
 	  alert("Foute wachtwoord!! Probeer opnieuw");
 	}
