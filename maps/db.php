fd35689edd94b38928bb26f3644c34db0523cc3d
Badr <70712849+bkhaie@users.noreply.github.com>
2022-01-12 20:36:11 +0100

    broken

---
+++ maps/db.php
@@ -0,0 +1,15 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="map";
+
+$conn=new mysqli($servername,$username,$password,$dbname);
+
+if($conn->connect_error){
+	die("Connection Failed".$conn->connect_error);
+}else{
+	//echo "connected";
+}
+
+?>
